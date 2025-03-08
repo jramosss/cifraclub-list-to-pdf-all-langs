82caa5491296256df62659d5613b27ecf75cc147
jramosss <jramostod@gmail.com>
2025-03-08 01:00:53 -0300

    pdf generation

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,10 +1,11 @@
+import { htmlToPdf } from './src/pdf';
 import Scraper from './src/scraper';
 
 
 async function main() {
     const scraper = new Scraper('1');
     const result = await scraper.scrape('https://www.cifraclub.com/musico/551928421/repertorio/12409416/');
-    console.log(result);
+    await htmlToPdf(result, 'output.pdf');
 }
 
 main().then(() => console.log('done')).catch(console.error);
---
+++ typescript/src/pdf.ts
@@ -0,0 +1,9 @@
+import playwright from 'playwright';
+
+export async function htmlToPdf(html: string, pdfPath: string) {
+    const browser = await playwright.chromium.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+    await page.pdf({ path: pdfPath });
+    await browser.close();
+}
