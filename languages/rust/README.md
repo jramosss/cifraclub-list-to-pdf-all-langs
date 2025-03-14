970305c7f6643c597ce2dbd4b32e8dcba428235c
jramosss <jramostod@gmail.com>
2025-03-14 12:54:32 -0300

    rust: trying to make dockerfile work but i'm way too tired

---
+++ languages/rust/README.md
@@ -0,0 +1,11 @@
+# Cifraclub to PDF - Rust - scraper + headless_chrome
+
+## How to run
+- cargo build --release
+- ./target/release/cifraclub_scraper
+
+## Conclusions:
+- So hard to learn, i understood nothing, everything here is difficult and so different to other languages, and it's not like i've never used a low level lang before, but this is nothing close to c++ even.
+- Tried to make such a "high level" feature with a low level language, not fun
+- Not fun at all, i did not enjoy this experiment
+- Compiling takes a long time, like 80 seconds to build a project with 7 dependencies
--- languages/rust/src/pdf.rs
+++ languages/rust/src/pdf.rs
@@ -8,13 +8,10 @@ pub async fn html_to_pdf(server_port: u16) {
 
     let tab = browser.new_tab().expect("Failed to create new tab");
 
-    // Navigate to wikipedia
     tab.navigate_to(&format!("http://localhost:{}/result", server_port)).expect("Failed to navigate to the result page");
-    let html = tab.get_content().expect("Failed to get the HTML page");
-    let mut file = File::create(format!("html_{}.html", server_port)).expect("Failed to create HTML file");
-    file.write_all(html.as_bytes()).expect("Failed to write to file");
 
     let pdf_bytes = tab.print_to_pdf(None);
+
     let mut file = File::create(format!("output_{}.pdf", server_port)).expect("Failed to create file");
     file.write_all(&pdf_bytes.unwrap()).expect("Failed to write to file");
 }
