b4a11b44597910a17c00ebb6ce24f4f50831315d
jramosss <jramostod@gmail.com>
2025-09-19 20:38:05 -0300

    adds puppeteer parameters to bun

--- languages/typescript-bun-puppeteer/src/pdf.ts
+++ languages/typescript-bun-puppeteer/src/pdf.ts
@@ -1,11 +1,18 @@
 import puppeteer from 'puppeteer';
 
 export async function htmlToPdf(html: string, pdfPath: string) {
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
-    await page.setContent(html);
-    await page.pdf({ path: pdfPath, format: 'A4' });
-    await browser.close();
+  const browser = await puppeteer.launch({
+    headless: true,
+    waitForInitialPage: false,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+    ],
+  });
+  const page = await browser.newPage();
+  await page.setContent(html);
+  await page.pdf({ path: pdfPath, format: 'A4' });
+  await browser.close();
 }
